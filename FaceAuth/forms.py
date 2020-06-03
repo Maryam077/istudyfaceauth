9f114f06333a9b75ab12a8243c4024b974c0bbb7
Maryam077 <maryamraafat077@gmail.com>
2020-06-03 20:42:14 +0200

    Registration

--- FaceAuth/forms.py
+++ FaceAuth/forms.py
@@ -1,11 +1,34 @@
+"""
+lib imports
+"""
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import UserProfile
+from .utils import base64_file
 
-
+"""
+Registration form
+"""
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
+    image = forms.ImageField()
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+    
+    def save(self, commit=True):
+        if not commit:
+            raise NotImplementedError("Can't create User and UserProfile without database save")
+        user = super(UserRegisterForm, self).save(commit=True)
+        image = base64_file(self.data['image'])
+        face_image = UserProfile(user=user, image=image)
+        face_image.save()
+        return user
+
+"""
+Authentication
+"""
+class UserAuthenticationForm(AuthenticationForm):
+    image = forms.CharField(widget=forms.HiddenInput())
--- FaceAuth/views.py
+++ FaceAuth/views.py
@@ -1,17 +1,25 @@
-from django.shortcuts import render
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserAuthenticationForm
+from django.conf import settings
+from .utils import prepare_image
+
 
 # Create your views here.
 def register(request):
-    if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
+    if  request.method == 'POST':
+        form = UserRegisterForm(request.POST, request.FILES)
+
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password2']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect(settings.LOGIN_REDIRECT_URL)
     else:
         form = UserRegisterForm()
+
+
     return render(request, 'registration/register.html', {'form': form})
