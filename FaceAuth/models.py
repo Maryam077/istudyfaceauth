087e052cea509e93e46d44897888a586383f98a6
Maryam077 <maryamraafat077@gmail.com>
2020-06-03 19:41:21 +0200

    initial commit

---
+++ FaceAuth/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
+# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=None, primary_key=True)
+    photo = models.ImageField(upload_to='profile_images', blank=True)
+    
+    class Meta:
+        ordering = ["user"]
+
+    def __str__(self):
+        return self.user.username
+
+def create_profile(sender,**kwargs):
+    if kwargs['created']:
+        user_profile=UserProfile.objects.get_or_create(user=kwargs['instance'])
+
+post_save.connect(create_profile,sender=User)
