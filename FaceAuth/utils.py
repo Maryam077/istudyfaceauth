21ebdd6be8bf3612a2209971758a0be04df53116
maryam raafat <maryamraafat077@gmail.com>
2020-06-23 19:08:27 +0200

    remove unnecessary functions

--- FaceAuth/utils.py
+++ FaceAuth/utils.py
@@ -52,84 +52,7 @@ def face_detect():
                 break
     return path
 
-def face_auth(location, username):
-    video_capture = cv2.VideoCapture(0)
-    capture_duration =5
-    
-    process_this_frame = True
-    known_face_names  = []
-    known_face_encodings = []
-    for prof in UserProfile.objects.all():
-        if len(prof.photo):
-            known_face_names.append(prof.user.username)
-            user_image = face_recognition.load_image_file(prof.photo)
-            user_face_encoding = face_recognition.face_encodings(user_image)[0]
-            known_face_encodings.append(user_face_encoding)
-    print(known_face_names)
-    #print(face_encodings)
-    start_time = time.time()
-
-    while True:
-        ret, frame = video_capture.read()
-
-        #for faster results
-        
-        small_frame = cv2.resize(frame, (0,0), fx=0.25, fy=0.25)
-
-        rgb_small_frame = small_frame[:,:,::-1]
-
-        if process_this_frame:
-            try:
-                face_locations = face_recognition.face_locations(rgb_small_frame)
-                face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-                face_names = []
-                name = 'Unknown'
-                for face_encoding in face_encodings:
-                    matches = face_recognition.compare_faces(known_face_encodings, face_encoding, tolerance=0.5)
-                    face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-                    best_match_index = np.argmin(face_distances)
-                    print(matches)
-                    if matches[best_match_index]:
-                        name = known_face_names[best_match_index]
-
-                    face_names.append(name)
-            except Exception as e:
-                print(e)
-        
-        if (int(time.time() - start_time) >= capture_duration):
-            process_this_frame = False
-            break
-
-        # Display the results
-        for (top, right, bottom, left), name in zip(face_locations, face_names):
-            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-            top *= 4
-            right *= 4
-            bottom *= 4
-            left *= 4
-
-            # Draw a box around the face
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-            # Draw a label with a name below the face
-            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-            font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-
-        # Display the resulting image
-        cv2.imshow('Video', frame)
-        cv2.waitKey(1)
-       
-    # Release handle to the webcam
-    video_capture.release()
-    cv2.destroyAllWindows()
-
-    if username in face_names:
-        return True
-    else:
-        return False
-
-def match_face(user):
+def match_face(user,show_window=False):
     user_image = face_recognition.load_image_file(user.userprofile.photo)
     user_face_encoding = face_recognition.face_encodings(user_image)[0]
     
@@ -154,16 +77,40 @@ def match_face(user):
                 
             matches = face_recognition.compare_faces(user_face_encoding, face_encoding, tolerance=0.5)
             face_distances = face_recognition.face_distance(user_face_encoding, face_encoding)
-            best_match_index = np.argmin(face_distances)
             print(matches)
-            if matches[best_match_index]:
+            if matches:
                 name = user.username
-                print(name)
                 face_names.append(name)
         except Exception as e:
             print(e)
         
         if (int(time.time() - start_time) >= duration):
             break
+        if show_window:
+        # Display the results
+            for (top, right, bottom, left), name in zip(face_locations, face_names):
+                # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+                top *= 4
+                right *= 4
+                bottom *= 4
+                left *= 4
+
+                # Draw a box around the face
+                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+                # Draw a label with a name below the face
+                cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+                font = cv2.FONT_HERSHEY_DUPLEX
+                cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+            # Display the resulting image
+            cv2.imshow('Video', frame)
+            cv2.waitKey(1)
+        
+    # Release handle to the webcam
     camera.release()
     cv2.destroyAllWindows()
+    if user.username in face_names:
+        return True
+    else:
+        return False
--- FaceAuth/views.py
+++ FaceAuth/views.py
@@ -9,24 +9,24 @@ from django.contrib.auth.models import User
 import threading
 from django.contrib.sessions.models import Session
 from django.utils import timezone
+from datetime import datetime
 
 def index(request):
-    if request.user.is_authenticated:
+    if request.user.is_authenticated  :
         checkloggedin(request.user)
     return render(request,'index.html',{})
 
 def checkloggedin(user):
-    sessions = Session.objects.filter(expire_date__gte=timezone.now())
-    for session in sessions:
-        data = session.get_decoded()
-        if data.get('_auth_user_id') and  data.get('_auth_user_id') == str(user.id):
-            print('matching')
-            match_face(user)
-            threading.Timer(60.0,checkloggedin,args=(user,)).start()
+    lastloginminutes = int(((timezone.now() - user.last_login).total_seconds())/60)%60
+    if lastloginminutes > 1:
+        sessions = Session.objects.filter(expire_date__gte=timezone.now())
+        for session in sessions:
+            data = session.get_decoded()
+            if data.get('_auth_user_id') and  data.get('_auth_user_id') == str(user.id):
+                print('matching')
+                match_face(user)
+                threading.Timer(60.0,checkloggedin,args=(user,)).start()
         
-    #user = User.objects.get(pk=id)
-    #print(user.is_authenticated)
-# Create your views here.
 def register(request):
     #face_detect('')
 
@@ -64,7 +64,7 @@ def login_page(request):
             user = authenticate(username=username, password=password)
             #print('user',user.userprofile.photo)
             if user:
-                if face_auth_view(user.userprofile.photo, user.username):
+                if face_auth_view(user):
                     login(request, user)
                     return redirect('index')
                 else:
@@ -81,9 +81,8 @@ def face_detect_view(request):
     x = face_detect()
     return JsonResponse({'data': x})
 
-def face_auth_view(location, username):
-    print(location)
-    x = face_auth(location, username)
+def face_auth_view(user):
+    x = match_face(user,True)
     print (x)
     return x
 
